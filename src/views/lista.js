7e409dd94ce507f6d156019e90d1d31477a124fb
Gean Feltrin <gean_costa@hotmail.com>
2018-11-11 23:50:10 -0300

    SectionList

--- src/views/lista.js
+++ src/views/lista.js
@@ -1,17 +1,37 @@
 import React,{Component} from 'react'
-import {Text, View, FlatList, StyleSheet} from 'react-native'
-import Boards from '../components/Boards'
+import {Text, View, SectionList, StyleSheet} from 'react-native'
 import api from '../services/api'
-import {Filho} from '../components/Cards/lists'
-// import Cards from '../components/Cards/cards'
 
+class SectionListItem extends Component{
+    render(){
+        return(
+            <View>
+                <Text>{this.props.item.name} {this.props.item.id}</Text>
+                
+            </View>
+           
+           )
 
+    }
+}
+
+class SectionListTitle extends Component{
+    render(){
+        return(
+            <View>
+                <Text style={{fontWeight: 'bold'}}>{this.props.section.title}</Text>
+                
+            </View>
+           
+           )
+
+    }
+}
 
 export default class Lista extends Component{
    state={
-        cards:{},
-        
-  
+        cards:[],
+          
    }
    
 static navigationOptions = ({ navigation }) => {
@@ -28,24 +48,35 @@ componentDidMount = () => {
 }
 
 loadCards = async (props) =>{
-    console.log(props)
-    const response = await api.get(`boards/${props}?cards=all&lists=all&list_field=id,name`)
-    this.setState({ cards: response.data })
-    console.log(response.data)
+    const response = await api.get(`boards/${props}/lists?cards=open&card_fields=name&filter=open&fields=name`)
 
+    const cards = response.data.map(card => ({
+        title: card.name,
+        data: card.cards
+    }))
+
+    this.setState({ cards })
 }
- 
 
    
-    render(){
-    const {cards , lists} = this.state.cards
-    
-    console.log(cards)
-
-       return(
-            <View><Text>Tes</Text>
-                
-                </View>
+render(){
+    const test = this.state.cards
+    console.log(test)
+      
+return(
+    <View>            
+    <SectionList
+        renderItem={({item, index, section}) => <Text key={index}>{item.name}</Text>}
+        renderSectionHeader={({section: {title}}) => (
+            <Text style={{fontWeight: 'bold'}}>{title}</Text>
+        )}
+        sections={this.state.cards}
+  keyExtractor={(item, index) => item + index}
+/>
+       
+          
+        
+    </View>
       )
 }
 }
--- src/views/main.js
+++ src/views/main.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import {View, Text} from 'react-native'
 
 
-
 export default class Main extends Component {
    
     static navigationOptions = {
