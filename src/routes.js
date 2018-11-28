8db9f08a20ba77c2cd40b34f5b3b9c02bbeea9f7
Gean Feltrin <gean_costa@hotmail.com>
2018-11-27 23:34:31 -0300

    login

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,11 @@ import { createStackNavigator } from "react-navigation";
 import Main from "./views/main";
 import Lista from "./views/lista";
 
+import Auth from "./views/auth";
+
 export default createStackNavigator(
   {
+    Auth,
     Main,
     Lista
   },
